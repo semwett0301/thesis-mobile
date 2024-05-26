a2b06cdbaacbbecf1b9d9f4d18fc227c41722233
semen.mokrov <semen.mokrov@gmail.com>
2024-05-26 18:05:14 +0300

    fix: open url

--- src/shared/utils/link.ts
+++ src/shared/utils/link.ts
@@ -1,14 +1,7 @@
-import { Toast } from "@ant-design/react-native";
 import { Linking } from "react-native";
 
 export const openLink = async (link?: string) => {
   if (link) {
-    const supported = await Linking.canOpenURL(link);
-
-    if (supported) {
-      await Linking.openURL(link);
-    } else {
-      Toast.fail(`Don't know how to open this URL: ${link}`);
-    }
+    await Linking.openURL(link);
   }
 };
